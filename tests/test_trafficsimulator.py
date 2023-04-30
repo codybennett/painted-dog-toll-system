487397bc685c4eef47e646c1e9ffa8e91e99566c
cody-bennett <74752788+cody-bennett@users.noreply.github.com>
2023-04-30 21:53:54 +0100

    Update Lock, add test

---
+++ tests/test_trafficsimulator.py
@@ -0,0 +1,14 @@
+import logging
+from unittest import TestCase
+from unittest.mock import patch
+
+logging.getLogger().setLevel(logging.DEBUG)
+
+import trafficsimulator
+
+
+class TestTrafficSimulator(TestCase):
+
+    def setUp(self):
+        pass
+
